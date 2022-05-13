0df8d61ea8d8a41dee16ad923750cdbf90d5d248
Srikar <srikarpanuganti64@gmail.com>
2022-05-13 14:37:26 +0530

    resume update #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,7 +95,7 @@ const Home: NextPage = ({posts}: { posts: EntryType[] }) => {
                   <span><Image src="/images/charminar.png" width={45} height={45} alt="Hyderabad, India" title="Hyderabad, India" /></span>
                   <span className={styles.heroWrapperDescOutline}>Hyd, India</span> with knack for building <br/>scalable faster <span className={styles.heroWrapperDescOutline}>web</span>.
                 </p>
-                <a href="https://srikar8968.github.io/Portfolio/resume.pdf" target="_blank" rel="noreferrer" className="txt-primary flex items-center">
+                <a href="/myresume.pdf" target="_blank" rel="noreferrer" className="txt-primary flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"/><path d="M13 2v7h7"/></svg>
                   <small className="font-alt txt-lg">&nbsp;View Resume</small>
                 </a>
